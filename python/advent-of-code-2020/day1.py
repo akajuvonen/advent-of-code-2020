912e26f8e6ad5f86261997c8d213284eaccbea38
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-04 23:43:03 +0900

    day 1 part 2

--- python/advent-of-code-2020/day1.py
+++ python/advent-of-code-2020/day1.py
@@ -7,11 +7,20 @@ def main():
         for line in f:
             entries.add(int(line.rstrip('\n')))
 
+    # two entries that sum up to 2020
     for entry in entries:
         remainder = 2020 - entry
         if remainder in entries:
-            print(entry * remainder)
-            return
+            print(f'{entry} and {remainder} multiplied {entry * remainder}')
+            break
+
+    # three entries that sum up to 2020
+    for first in entries:
+        for second in entries:
+            remainder = 2020 - first - second
+            if remainder in entries:
+                print(f'{first}, {second}, {remainder} multiplied: {first * second * remainder}')
+                return
 
 
 if __name__ == '__main__':
