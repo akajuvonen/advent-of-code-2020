f250b579abffd5c3632a105aecf4fcbf5be23205
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-05 23:41:06 +0900

    Clojure project template

---
+++ src/com/github/akajuvonen/advent_of_code.clj
@@ -0,0 +1,7 @@
+(ns com.github.akajuvonen.advent-of-code
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/com/github/akajuvonen/advent_of_code_test.clj
@@ -0,0 +1,7 @@
+(ns com.github.akajuvonen.advent-of-code-test
+  (:require [clojure.test :refer :all]
+            [com.github.akajuvonen.advent-of-code :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
