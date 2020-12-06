40ddbfc17e5daf49dbd26c14c04a8167983d59f0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-06 22:54:39 +0900

    Parse file into ints

--- src/com/github/akajuvonen/advent_of_code.clj
+++ src/com/github/akajuvonen/advent_of_code.clj
@@ -1,7 +1,11 @@
 (ns com.github.akajuvonen.advent-of-code
   (:gen-class))
 
+(defn parse-file [filename]
+  (let [arr (clojure.string/split (slurp filename) #"\n")]
+    (map #(Integer/parseInt %) arr)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (parse-file "inputs/day1.txt")))
