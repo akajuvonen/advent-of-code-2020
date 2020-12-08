2f01dfeba76111849afdee5c4d288cda28659810
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-08 22:40:23 +0900

    Rename and refactor project files

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,9 +1,9 @@
-(ns com.github.akajuvonen.advent-of-code
-  (:gen-class))
+(ns advent-of-code.core
+  (:use [clojure.math.combinatorics :only (combinations)]))
 
-(defn parse-file [filename]
-  (let [arr (clojure.string/split (slurp filename) #"\n")]
-    (map #(Integer/parseInt %) arr)))
+(defn parse [string]
+  (clojure.string/split (string) #"\n"))
+  
 
 (defn day1-part1
   "Find the multiple of two entries that sum up to 2020"
@@ -18,5 +18,4 @@
 (defn -main
   "Advent of code solutions main."
   [& args]
-  (def entries (set (parse-file "inputs/day1.txt")))
-  (println (day1-part1 entries)))
+  (parse (slurp "inputs/day1.txt")))
