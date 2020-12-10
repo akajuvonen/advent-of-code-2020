81adf5dd7cfa0f1200221517bd26855f47932a2f
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-10 22:40:49 +0900

    Add a test blueprint to be used later

---
+++ test/advent_of_code/day01_test.clj
@@ -0,0 +1,7 @@
+(ns advent-of-code.day01-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.day01 :refer :all]))
+
+(deftest part1-test
+  (testing "FIXME, I fail."
+    (is (= 1 0))))
