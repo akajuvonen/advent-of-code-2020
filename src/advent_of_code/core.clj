5c34014f4d39186729d6ae8357f454f138b44b16
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-13 00:59:01 +0900

    Add day 2 in core

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,8 +1,10 @@
 (ns advent-of-code.core
-  (:require [advent-of-code.day01])
+  (:require [advent-of-code.day01]
+            [advent-of-code.day02])
   (:gen-class))
 
 (def input-day1 "inputs/day01.txt")
+(def input-day2 "inputs/day02.txt")
 
 (defn -main
   "Advent of code solutions."
@@ -11,4 +13,7 @@
   (println "Part 1")
   (println (advent-of-code.day01/part1 (slurp input-day1)))
   (println "Part 2")
-  (println (advent-of-code.day01/part2 (slurp input-day1))))
+  (println (advent-of-code.day01/part2 (slurp input-day1)))
+  (println "Day 2")
+  (println "Part 1")
+  (println (advent-of-code.day02/part1 (slurp input-day2))))
