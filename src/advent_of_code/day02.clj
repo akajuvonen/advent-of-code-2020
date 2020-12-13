5cdea84fc97a439de3908e439a062b20068fefce
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-13 14:31:52 +0900

    Use get not-found instead of custom function

--- src/advent_of_code/day02.clj
+++ src/advent_of_code/day02.clj
@@ -1,18 +1,13 @@
 (ns advent-of-code.day02
   (:require [clojure.string :as string]))
 
-(defn get-char-freq 
-  [passwd char]
-  (let [freq (get (frequencies passwd) char)]
-    (if (nil? freq) 0 freq)))
-
 (defn validate-passwd
   "Validate that character count in passwd is in range [lowest, highest]. TODO: handle nill in freqs"
   [lowest 
    highest 
    character
    passwd]
-  (<= lowest (get-char-freq passwd character) highest))
+  (<= lowest (get (frequencies passwd) character 0) highest))
 
 (defn parse-line
   [line]
