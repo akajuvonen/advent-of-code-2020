54ab99601015a9b024d0635176646457e5d953cf
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-16 22:19:15 +0900

    Part 1 solution

--- src/advent_of_code/day04.clj
+++ src/advent_of_code/day04.clj
@@ -24,9 +24,17 @@
        (into {})
        (walk/keywordize-keys)))
 
+(def required-keys [:byr :iyr :eyr :hgt :hcl :ecl :pid])
+
 (defn part1
   [input]
   (->> input
        (separate-passports)
        (map separate-fields)
-       (map fields->map)))
+       (map fields->map)
+       (map #(every? % required-keys))
+       (filter identity)
+       (count)))
+
+(let [m {:hcl "#a7a05c", :pid "0137262572", :eyr "2023", :cid "350", :iyr "2015", :ecl "#52d3fe", :hgt "190cm", :byr "2007"}]
+  (every? m required-keys))
