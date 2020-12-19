9c32ee88458fe80d6678f01d32c93dd2679e4baf
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-19 23:18:21 +0900

    Add func for getting seat id

--- src/advent_of_code/day05.clj
+++ src/advent_of_code/day05.clj
@@ -13,13 +13,17 @@
         (recur lower (- upper diff) remaining lower-char)
         (recur (+ lower diff) upper  remaining lower-char)))))
 
-(defn get-row-and-column
+(defn get-seat-id
   [s]
   (let [row-string (subs s 0 7)
-        column-string (subs s 7 10)]
-    (dec (binary-space-partition 1 128 row-string \F))))
+        column-string (subs s 7 10)
+        row (dec (binary-space-partition 1 128 row-string \F))
+        column (dec (binary-space-partition 1 8 column-string \L))]
+    (+ (* 8 row) column)))
 
 (defn part1
   [input]
   (-> input
       (slurp)))
+
+(get-seat-id "FBFBBFFRLR")
