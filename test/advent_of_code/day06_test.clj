e7166f733b63e15cf8af7eadf4e08c582f4e9efe
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-21 22:29:17 +0900

    Add test for getting common chars in a sequence of strings

--- test/advent_of_code/day06_test.clj
+++ test/advent_of_code/day06_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer [deftest is testing]]
             [advent-of-code.day06]))
 
-(deftest calculate-seat-id
+(deftest test-uniq-count
   (testing "uniq-count"
     (is (= 5 (advent-of-code.day06/uniq-count ["abc" "cde"])))))
+
+(deftest test-calc-common-chars
+  (testing "common-chars"
+    (is (= #{\c} (advent-of-code.day06/common-chars ["abc" "cde"])))))
