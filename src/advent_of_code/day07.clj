f657d05dcbaa5242693d595c5d9d76d9d61ffd52
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-23 22:39:22 +0900

    Parse input bags, and bags contained inside

--- src/advent_of_code/day07.clj
+++ src/advent_of_code/day07.clj
@@ -1,8 +1,12 @@
-(ns advent-of-code.day07)
+(ns advent-of-code.day07
+  (:require [clojure.string :as str]))
 
 (defn parse
   [input]
-  (let [bag (second (re-find #"^(.*)\ bags\ contain" input))]
-    bag))
+  (let [bag (second (re-find #"^(.*)\ bags\ contain" input))
+        leftover-string (second (str/split input #"contain\ "))
+        included-bag-strings (str/split leftover-string #",\ ")
+        included-bag-colors (map second (map #(re-find #"\d\ (.*)\ " %) included-bag-strings))]
+    [bag included-bag-colors]))
 
 (parse "mirrored white bags contain 1 bright gray bag, 4 plaid blue bags.")
