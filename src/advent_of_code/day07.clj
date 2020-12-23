d30a1135c8d4c055c6a1a721b0922f3e41d28d1b
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-23 22:11:48 +0900

    Parse bag name

---
+++ src/advent_of_code/day07.clj
@@ -0,0 +1,8 @@
+(ns advent-of-code.day07)
+
+(defn parse
+  [input]
+  (let [bag (re-find #"^(.*)\ bags\ contain" input)]
+    bag))
+
+;(parse "mirrored white bags contain 1 bright gray bag, 4 plaid blue bags.")
