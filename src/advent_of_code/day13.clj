e14b995e5f2fc46dda1b444648f3efbcebb0ada4
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-12-26 22:13:58 +0900

    Refactor id departure time calculation to its own func

--- src/advent_of_code/day13.clj
+++ src/advent_of_code/day13.clj
@@ -12,12 +12,17 @@
         bus-ids (map #(Integer. %) bus-id-filtered)]
     [timestamp bus-ids]))
 
-(defn departures-by-id
+(defn departure-by-id
+  "Calculate time to next departure for a bus id."
+  [timestamp id]
+  (- id (mod timestamp id)))
+
+(defn all-departures-by-id
   "Calculate pairs containing id and time to that id's next departure."
   [timestamp ids]
-  (let [departures (map #(- % (mod timestamp %)) ids)]
+  (let [departures (map #(departure-by-id timestamp %) ids)]
     (map vector ids departures)))
 
 (let [[timestamp bus-ids] (parse test-input)]
-  (departures-by-id timestamp bus-ids))
+  (all-departures-by-id timestamp bus-ids))
 
