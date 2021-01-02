373f1aed664678a8288f86cb6570887c3e01f653
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-02 22:32:29 +0900

    CHORE: remove unused defs

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -8,14 +8,6 @@
             [advent-of-code.day13])
   (:gen-class))
 
-(def input-day1 "inputs/day01.txt")
-(def input-day2 "inputs/day02.txt")
-(def input-day4 "inputs/day04.txt")
-(def input-day5 "inputs/day05.txt")
-(def input-day6 "inputs/day06.txt")
-(def input-day7 "inputs/day07.txt")
-(def input-day13 "inputs/day13.txt")
-
 (defn- pad-int-string
   "Pad string x if just one digit, e.g., 1 -> 01.
    Always return string."
