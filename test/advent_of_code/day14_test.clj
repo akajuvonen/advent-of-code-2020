50015179bcdc5b4c25e5fe2c9d52d64cf9898caf
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-02 21:57:50 +0900

    Add binary string pad test

--- test/advent_of_code/day14_test.clj
+++ test/advent_of_code/day14_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer [deftest is testing]]
             [advent-of-code.day14]))
 
+(deftest test-pad-binary-string
+  (testing "pad-binary-string"
+    (is (= "00001011"
+           (advent-of-code.day14/pad-binary-string "1011" 8)))))
+
 (deftest test-apply-bitmask
   (testing "apply-bitmask"
     (is (= "1101"
