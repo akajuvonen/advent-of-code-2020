337182b0bdef28941b2920f2c75b4998e03f523b
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-02 21:53:13 +0900

    Add bitmask func

--- src/advent_of_code/day14.clj
+++ src/advent_of_code/day14.clj
@@ -7,10 +7,26 @@
         padding (apply str (take padding-size (repeat "0")))]
     (str padding string)))
 
+(defn- mask-bit
+  "Apply a bit mask to one bit. `X` means return original bit, otherwise mask."
+  [bit mask]
+  (if (= mask \X)
+    bit
+    mask))
+
+(defn apply-bitmask
+  "Apply a bitmask to a binary string. `X` means that the bit is unchanged,
+   otherwise changed to the corresponding value in mask. E.g.,
+   number = 1011, mask = X1XX -> 1111"
+  [number mask]
+  (apply str (map mask-bit number mask)))
+
 (Integer/toBinaryString 11)
 (Integer/parseInt "1011" 2)
 
 (let [s "1011"]
   (pad-binary-string s 36))
 
-
+(let [number "1011"
+      mask "X1XX"]
+  (apply-bitmask number mask))
