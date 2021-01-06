f8f43d3f7a7b33d82792ea1ed444fdb7da58dc7c
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-06 22:10:19 +0900

    Parse nearby tickets

--- src/advent_of_code/day16.clj
+++ src/advent_of_code/day16.clj
@@ -12,11 +12,21 @@
                     (apply range
                            (mapv #(Integer. %) (str/split rule #"-"))))))))
 
+(defn- parse-nearby-tickets
+  [nearby-tickets]
+  (->> nearby-tickets
+       str/split-lines
+       (drop 1)
+       (map #(str/split % #","))
+       flatten
+       (map #(Integer. %))))
+
 (defn parse
   [input]
   (let [[rules your-ticket nearby-tickets] (str/split input #"\n\n")
-        rules (parse-rules (str/split-lines rules))]
-    rules))
+        rules (parse-rules (str/split-lines rules))
+        nearby-tickets (parse-nearby-tickets nearby-tickets)]
+    [rules nearby-tickets]))
 
 (let [input (slurp "inputs/day16.txt")]
   (-> input
