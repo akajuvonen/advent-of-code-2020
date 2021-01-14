96821d61b1ccf963d3c09576008ba458108e4ec5
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-14 22:02:42 +0900

    Eval a single arithmetic expression

--- src/advent_of_code/day18.clj
+++ src/advent_of_code/day18.clj
@@ -1,6 +1,18 @@
 (ns advent-of-code.day18
   (:require [clojure.walk :as walk]))
 
+(defn- infix
+  [left [op right]]
+  ((resolve op) left right))
+
+(defn evaluate
+  "Evaluate a list containing arithmetic expressions, e.g.,
+   `(1 + 2 * 3)`."
+  [[acc & remaining]]
+  (reduce infix acc (partition 2 remaining)))
+
+(evaluate (read-string "(1 + 2 * 3)"))
+
 (defn part1
   [input]
   (slurp input))
