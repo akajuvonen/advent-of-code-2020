3b447c4c6501d7d51c0b81f7371080e41014f76f
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-19 22:05:34 +0900

    Add play game funcion

--- src/advent_of_code/day22.clj
+++ src/advent_of_code/day22.clj
@@ -13,4 +13,20 @@
     (map #(drop 1 %) i)
     (map #(seq->intvec %) i)))
 
-(parse (slurp "inputs/day22.txt"))
+(defn play-game
+  [deck1 deck2]
+  (if (empty? deck2)
+    deck1
+    (if (empty? deck1)
+      deck2
+      (let [card1 (first deck1)
+            card2 (first deck2)]
+        (if (> card1 card2)
+          (recur (conj  (drop 1 deck1) card1 card2) (drop 1 deck2))
+          (recur (drop 1 deck1) (conj  (drop 1 deck2) card2 card1)))))))
+
+(let [[deck1 deck2] (parse (slurp "inputs/day22.txt"))]
+  (play-game deck1 deck2))
+
+(conj [1 2] 3 4)
+(subvec [1 2 3] 1)
