4530c4e4927b01af49144f66b6dce0da60d39a82
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-19 22:23:43 +0900

    DOCS: add docstrings

--- src/advent_of_code/day22.clj
+++ src/advent_of_code/day22.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code.day22
   (:require [clojure.string :as str]))
 
-(defn seq->intvec
+(defn- seq->intvec
   [l]
   (mapv #(Integer. %) l))
 
@@ -14,6 +14,9 @@
      (map #(seq->intvec %)))))
 
 (defn play-game
+  "Play a game where one card is drawn from both decks. Player with larger
+   number gets both added to his/her deck. Player wins when other player's
+   deck is empty."
   [deck1 deck2]
   (if (empty? deck2)
     deck1
@@ -26,6 +29,8 @@
           (recur (subvec deck1 1) (conj  (subvec deck2 1) card2 card1)))))))
 
 (defn calculate-score
+  "Calculate deck's score. E.g., deck is [4 5 6], score is
+   3 * 4 + 2 * 5 + 1 * 6."
   [deck]
   (reduce + (map #(reduce * %)
                  (map vector deck (range (count deck) 0 -1)))))
