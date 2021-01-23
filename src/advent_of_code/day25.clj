35c685a5564e704922068cc851a7bcc42fe661c9
Antti Juvonen <akajuvonen@users.noreply.github.com>
2021-01-23 21:45:58 +0900

    Parse input

--- src/advent_of_code/day25.clj
+++ src/advent_of_code/day25.clj
@@ -1,8 +1,15 @@
 (ns advent-of-code.day25
   (:require [clojure.string :as str]))
 
+(defn parse
+  [input]
+  (->> input
+      str/split-lines
+      (map #(Integer. %))))
+
 (defn transform
   [value]
   (rem  (* value 7) 20201227))
 
-(transform 100000000)
+(let [[door card] (parse (slurp "inputs/day25.txt"))]
+  [door card])
